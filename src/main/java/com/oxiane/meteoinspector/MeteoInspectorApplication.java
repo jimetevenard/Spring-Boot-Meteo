f5fc114fddb411e16bca8c5d61e4db314c44022d
Jim ETEVENARD <jetevenard@oxiane.com>
2023-04-29 18:17:55 +0200

    Init with start.spring.io

---
+++ src/main/java/com/oxiane/meteoinspector/MeteoInspectorApplication.java
@@ -0,0 +1,13 @@
+package com.oxiane.meteoinspector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeteoInspectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeteoInspectorApplication.class, args);
+	}
+
+}
